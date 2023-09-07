835e8d883edb0f1144268a19f5d3b6aa70e64473
Woogie-94 <dmsdmr9411@gmail.com>
2023-09-07 20:22:07 +0900

    feat: Query 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 
+import QueryClient from "./core/queryClient";
 import router from "./routes/route";
 import GlobalStyle from "./styles/globalStyle";
+
+export const queryClient = new QueryClient();
 
 function App() {
   return (
